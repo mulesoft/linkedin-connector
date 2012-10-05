4be63bea8499c0c74ce7d6a6d4377910c8b2b307
Jose Sahad <jose.amenta@mulesoft.com>
2012-10-05 17:31:28 -0300

    Fix FacetType issue

--- src/main/java/com/google/code/linkedinapi/schema/FacetType.java
+++ src/main/java/com/google/code/linkedinapi/schema/FacetType.java
@@ -10,45 +10,14 @@
 
 package com.google.code.linkedinapi.schema;
 
-import javax.xml.bind.annotation.XmlEnum;
-import javax.xml.bind.annotation.XmlEnumValue;
-
-
-/**
- * <p>Java class for null.
- * <p/>
- * <p>The following schema fragment specifies the expected content contained within this class.
- * <p/>
- * <pre>
- * &lt;simpleType>
- *   &lt;restriction base="{http://www.w3.org/2001/XMLSchema}string">
- *     &lt;enumeration value="location"/>
- *     &lt;enumeration value="industry"/>
- *     &lt;enumeration value="network"/>
- *     &lt;enumeration value="language"/>
- *     &lt;enumeration value="current-company"/>
- *     &lt;enumeration value="past-company"/>
- *     &lt;enumeration value="school"/>
- *   &lt;/restriction>
- * &lt;/simpleType>
- * </pre>
- */
-@XmlEnum
 public enum FacetType {
 
-    @XmlEnumValue("location")
     LOCATION("location"),
-    @XmlEnumValue("industry")
     INDUSTRY("industry"),
-    @XmlEnumValue("network")
     NETWORK("network"),
-    @XmlEnumValue("language")
     LANGUAGE("language"),
-    @XmlEnumValue("current-company")
     CURRENT_COMPANY("current-company"),
-    @XmlEnumValue("past-company")
     PAST_COMPANY("past-company"),
-    @XmlEnumValue("school")
     SCHOOL("school");
     private final String value;
 
